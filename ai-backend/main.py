36366fcba27be9e4edfc5fc9c47ab145a9ecc541
Aryan <sinhaaryan173@gmail.com>
2026-05-27 12:50:41 +0530

    initial commit: consolidate frontend, backend, and docs under single repo

---
+++ ai-backend/main.py
@@ -0,0 +1,35 @@
+from fastapi import FastAPI
+from groq import Groq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+app = FastAPI()
+
+client = Groq(
+    api_key=os.getenv("GROQ_API_KEY")
+)
+
+@app.get("/")
+def home():
+    return {
+        "message": "EchoVerse AI Backend Running"
+    }
+
+@app.post("/chat")
+def chat(prompt: str):
+
+    completion = client.chat.completions.create(
+        model="llama3-8b-8192",
+        messages=[
+            {
+                "role": "user",
+                "content": prompt
+            }
+        ]
+    )
+
+    return {
+        "response": completion.choices[0].message.content
+    }
